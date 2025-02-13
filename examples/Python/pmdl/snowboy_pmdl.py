046d9b688577c529e37c0f9a250d269a3155dea1
ipaubikare <ialbeniz@ubikare.io>
2025-02-13 16:41:04 +0100

    Update snowboy_pmdl.py
    add condition for collab machine

--- examples/Python/pmdl/snowboy_pmdl.py
+++ examples/Python/pmdl/snowboy_pmdl.py
@@ -8,5 +8,8 @@ if platform.system() == "Darwin":
 elif platform.linux_distribution()[0] == "Ubuntu" and platform.linux_distribution()[1] == "16.04":
     sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/ubuntu64/pmdl"))
     from snowboy import *
+elif platform.system() == "Linux" and platform.machine() == "x86_64":
+    sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/ubuntu64/pmdl"))
+    from snowboy import *
 else:
     raise ImportError("pmdl generator only runs on OSX or Ubuntu 16.04.")
